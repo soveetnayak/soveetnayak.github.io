55e8df74f11a0b57e2ebfb04228e92b7d07bdc6f
Soveet Nayak <soveet.nayak@gmail.com>
2024-02-14 21:44:23 +0530

    vday

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Page from './page.tsx'; // Make sure this path is correct
+
+function App() {
+  return (
+    <div className="App">
+      <Page />
+    </div>
+  );
+}
+
+export default App;
